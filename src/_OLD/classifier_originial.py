df2aebe60cda320322c9c275c35c97f6d85a0e98
bettini <florian.bettini@gmail.com>
2020-04-13 18:19:14 +0200

    classifier & tester

---
+++ src/_OLD/classifier_originial.py
@@ -0,0 +1,23 @@
+__authors__ = ['']
+
+#Importing modules
+import os
+import sys
+import time
+import torch
+import torch.nn.functional as F
+import numpy as np
+
+
+class Classifier:
+    """The Classifier"""
+        
+    #############################################
+    def train(self, trainfile):
+        """Trains the classifier model on the training set stored in file trainfile"""
+
+
+    def predict(self, datafile):
+        """Predicts class labels for the input instances in file 'datafile'
+        Returns the list of predicted labels
+        """
